b5d6675a8937e52669685a2216408fcc6d8f38c8
Eduard-2709 <98168569+Eduard-2709@users.noreply.github.com>
2022-02-10 01:21:11 -0800

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,7 @@
+#pragma once
+int size();
+void inputmasa(int**, int, int);
+void inputmasb(int**, int, int);
+void showmasa(int**, int, int);
+void showmasb(int**, int, int);
+void multimas(int**, int**, int**, int, int, int, int);
